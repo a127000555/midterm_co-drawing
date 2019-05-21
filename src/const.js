4484f250070f4aa32ef7162a1a5576fc41f93039
Arvin Liu <a127000555@gmail.com>
2019-05-21 16:34:23 +0800

    change server.js

--- src/const.js
+++ src/const.js
@@ -1,3 +1,2 @@
-export const server_port = 8000;
-export const server_url = "http://localhost:8000";
-export const db_connect = "mongodb+srv://arvin:a127000555@cluster0-kzyxa.gcp.mongodb.net/test?retryWrites=true";
+export const server_port = 8787;
+export const server_url = "http://localhost:8787";
